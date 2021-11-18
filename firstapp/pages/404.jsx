805e9a258cb6f6c388a5bb03f2cc77dbd657c9c0
Abhinav Namdeo <abhaynam22@gmail.com>
2021-11-18 21:47:03 +0530

    Routing without Link tag with onclick listner

--- firstapp/pages/404.jsx
+++ firstapp/pages/404.jsx
@@ -1,9 +1,12 @@
+import { useRouter } from 'next/dist/client/router'
 import React from 'react'
 
 const error = () => {
+    const router = useRouter();
     return (
         <div>
             <h1>Sorry, 404 not found</h1>
+            <button><a onClick={()=>{router.push("/")}}>Home</a></button>
         </div>
     )
 }
