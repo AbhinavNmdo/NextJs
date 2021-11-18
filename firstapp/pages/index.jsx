1529762e494930791b13386bce1b462b65202838
Abhinav Namdeo <abhaynam22@gmail.com>
2021-11-18 22:30:13 +0530

    Images with external and internal source

--- firstapp/pages/index.jsx
+++ firstapp/pages/index.jsx
@@ -6,6 +6,10 @@ import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
+    <>
     <Navbar/>
+    <Image src="/shop.jpg" width="500" height="500"></Image>        {/* {Internal Storage Source} */}
+    <Image src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" width="500" height="300"></Image>
+    </>
   )
 }
