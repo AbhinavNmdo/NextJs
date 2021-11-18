ed18d087a95e9d19ee8a015c66abc5cc5abd68ec
Abhinav Namdeo <abhaynam22@gmail.com>
2021-11-18 21:14:59 +0530

    Using useRouter Hook

---
+++ firstapp/pages/[itemNo].jsx
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/dist/client/router'
+import React from 'react'
+
+const itemNo = () => {
+    const router = useRouter();
+    const slug = router.query.itemNo;
+    return (
+        <div>
+            <h1>{slug}</h1>
+        </div>
+    )
+}
+
+export default itemNo
