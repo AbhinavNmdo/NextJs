0d8f76f8b22c956d553a55dfef409b0d67961f0f
Abhinav Namdeo <abhaynam22@gmail.com>
2021-11-19 10:52:24 +0530

    Fetching Api

--- firstapp/pages/Features.jsx
+++ firstapp/pages/Features.jsx
@@ -2,15 +2,31 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Head from 'next/head'
 
-const Features = () => {
+export const getStaticProps = async ()=>{
+    const responce = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const data = await responce.json();
+
+    return {
+        props: { data }
+      }
+    }
+
+const Features = ({data}) => {
     return (
         <>
+        <Navbar/>
         <Head>
             <title>Features</title>
         </Head>
         <div>
-            <Navbar/>
-            <h1>This is Features</h1>
+            {data.map((cur)=>{
+                return(
+                    <div>
+                        <h2>{cur.id}</h2>
+                        <h1>{cur.title}</h1>
+                    </div>
+                )
+            })}
         </div>
         </>
     )
